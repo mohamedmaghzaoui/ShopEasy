967398183d1f08399b4bb7000836ef333b8ac475
ramla <ramla.argui2001@gmail.com>
2026-02-22 22:22:13 +0100

    Add Reviews and Payments pages

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AppNavbar } from "../AppNavbar";
-
-// Importer tes composants de pages
+import { AppNavbar } from "../AppNavbar"; 
 import { Users } from "./pages/User";
+import { Reviews } from "./pages/Reviews";
+import { Payments } from "./pages/Payments";
 
 export default function App() {
   return (
     <Router>
       <AppNavbar />
+
       <div className="container mt-3">
         <Routes>
-          <Route path="/users" element={<Users />} />
           <Route path="/" element={<h2>Welcome to MyApp</h2>} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/payments" element={<Payments />} />
         </Routes>
       </div>
     </Router>
