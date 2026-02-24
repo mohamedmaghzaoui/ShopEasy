6431d245e88a49822079571ce8a48dacaedaef4b
ramla <ramla.argui2001@gmail.com>
2026-02-24 16:49:51 +0100

    update meétier

---
+++ back/buisness/business.py
@@ -0,0 +1,23 @@
+from database import get_connection
+
+def get_top_clients():
+    conn = get_connection()
+    query = """
+    SELECT 
+        u.UserId,
+        u.FirstName,
+        u.LastName,
+        SUM(od.Quantity * od.UnitPrice) AS TotalSpent,
+        COUNT(o.OrderId) AS NumberOfOrders
+    FROM Users u
+    JOIN Orders o 
+        ON u.UserId = o.UserId
+    JOIN Order_Details od 
+        ON o.OrderId = od.OrderId
+    GROUP BY u.UserId
+    ORDER BY TotalSpent DESC
+    LIMIT 5
+    """
+    clients = conn.execute(query).fetchall()
+    conn.close()
+    return [dict(c) for c in clients]
---
+++ front/src/pages/Buisness.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export const Buisness = () => {
+  const [topCustomers, setTopCustomers] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/users/analyse/top-clients")
+      .then(res => setTopCustomers(res.data))
+      .catch(err => console.error("Erreur top clients:", err));
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h2 > Top 5 Clients les Plus Rentables</h2>
+
+      <table className="table table-bordered mt-4">
+        <thead className="table-light">
+          <tr>
+            <th>ID</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Total Dépensé (€)</th>
+            <th>Nombre de Commandes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {topCustomers.map(c => (
+            <tr key={c.UserId}>
+              <td>{c.UserId}</td>
+              <td>{c.LastName}</td>
+              <td>{c.FirstName}</td>
+              <td>{c.TotalSpent} €</td>
+              <td>{c.NumberOfOrders}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
